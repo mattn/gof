ad711f378c7673ce0562a0c1f3c6e0e05a26f0c8
Tasuku SUENAGA a.k.a. gunyarakun <tasuku-s-github@titech.ac>
2013-12-09 17:32:03 -0800

    Add tty_bsd.go for Mac OS X/FreeBSD

---
+++ tty_linux.go
@@ -0,0 +1,22 @@
+// +build linux
+
+package main
+
+import (
+  "os"
+  "syscall"
+  "unsafe"
+)
+
+func isatty() bool {
+  var termios syscall.Termios
+  _, _, err := syscall.Syscall6(syscall.SYS_IOCTL, os.Stdin.Fd(), uintptr(syscall.TCGETS), uintptr(unsafe.Pointer(&termios)), 0, 0, 0)
+  return err == 0
+}
+
+func tty_ready() error {
+  return nil
+}
+
+func tty_term() {
+}
